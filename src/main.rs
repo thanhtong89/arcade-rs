b7638e42e680a446b439b9565b58070dead208f7
Tong Pham <thanhtong89@gmail.com>
2017-04-30 21:07:12 -0700

    Step 5: now moving initializeation logic into phi::spawn, main only calls on spawn with closure pointing to new DefaultView

--- src/main.rs
+++ src/main.rs
@@ -3,51 +3,8 @@ extern crate sdl2;
 mod phi;
 mod views;
 
-use phi::{Phi, View, ViewAction};
-use views::DefaultView;
-
 fn main() {
-    // Initialize SDL2
-    let sdl_context = sdl2::init().unwrap();
-    let video = sdl_context.video().unwrap();
-    let mut timer = sdl_context.timer().unwrap();
-
-    // Create the window
-    let window = video.window("ArcadeRS Shooter", 800, 600)
-        .position_centered().opengl()
-        .build().unwrap();
-
-    // create context
-    let mut context = Phi::new(sdl_context, window);
-    let mut current_view = Box::new(DefaultView);
-
-    // Frame timimg
-    let interval = 1000 / 60;
-    let mut before = timer.ticks();
-    let mut last_second = timer.ticks();
-    let mut fps = 0u16;
-
-    loop {
-        let now = timer.ticks();
-        let dt = now - before;
-        let elapsed = dt as f64 / 1000.0;
-
-        if dt < interval {
-            timer.delay(interval - dt);
-            continue;
-        }
-
-        before = now;
-        fps += 1;
-        if now - last_second > 1000 {
-            println!("FPS: {}", fps);
-            last_second = now;
-            fps = 0;
-        }
-        context.events.pump();
-        match (*current_view).render(&mut context, elapsed) {
-            ViewAction::Quit => break,
-            ViewAction::None => context.renderer.present()
-        }
-    }
+    ::phi::spawn("ArcadeRS Shooter", |_| {
+        Box::new(::views::DefaultView)
+    });
 }
--- src/phi/mod.rs
+++ src/phi/mod.rs
@@ -52,3 +52,78 @@ pub trait View {
     // `elapsed` is expressed in seconds.
     fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction;
 }
+/// Create a window with name `title`, initialize the underlying libraries and
+/// start the game with the `View` returned by `init()`.
+///
+/// # Examples
+///
+/// Here, we simply show a window with color #ffff00 and exit when escape is
+/// pressed or when the window is closed.
+///
+/// ```
+/// struct MyView;
+///
+/// impl View for MyView {
+///     fn render(&mut self, context: &mut Phi, _: f64) -> ViewAction {
+///         if context.events.now.quit {
+///             return ViewAction::Quit;
+///         }
+///
+///         context.renderer.set_draw_color(Color::RGB(255, 255, 0));
+///         context.renderer.clear();
+///         ViewAction::None
+///     }
+/// }
+///
+/// spawn("Example", |_| {
+///     Box::new(MyView)
+/// });
+/// ```
+pub fn spawn<F>(title: &str, init: F)
+where F: Fn(&mut Phi) -> Box<View> {
+    // Initialize SDL2
+    let sdl_context = sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+    let mut timer = sdl_context.timer().unwrap();
+
+    // Create the window
+    let window = video.window("ArcadeRS Shooter", 800, 600)
+        .position_centered().opengl()
+        .build().unwrap();
+
+    // Create context
+    let mut context = Phi::new(sdl_context, window);
+
+    // Create the default view
+    let mut current_view = init(&mut context);
+
+    // Frame timimg
+    let interval = 1000 / 60;
+    let mut before = timer.ticks();
+    let mut last_second = timer.ticks();
+    let mut fps = 0u16;
+
+    loop {
+        let now = timer.ticks();
+        let dt = now - before;
+        let elapsed = dt as f64 / 1000.0;
+
+        if dt < interval {
+            timer.delay(interval - dt);
+            continue;
+        }
+
+        before = now;
+        fps += 1;
+        if now - last_second > 1000 {
+            println!("FPS: {}", fps);
+            last_second = now;
+            fps = 0;
+        }
+        context.events.pump();
+        match (*current_view).render(&mut context, elapsed) {
+            ViewAction::Quit => break,
+            ViewAction::None => context.renderer.present()
+        }
+    }
+}
