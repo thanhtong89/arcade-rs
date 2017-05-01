8bb5c12d11ebd0593a27d9f2c4b0c687515bbe8b
Tong Pham <thanhtong89@gmail.com>
2017-04-30 21:40:52 -0700

    Step 5: fix unnecessary creation of Box<View> of next view every render()

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -3,30 +3,36 @@ use phi::{Phi, View, ViewAction};
 pub struct ViewA;
 pub struct ViewB;
 
-fn render_template(context: &mut Phi, elapsed: f64, color: Color,
-                    next_view: Box<View>) -> ViewAction {
+impl View for ViewA {
+    fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction {
         if context.events.now.quit ||
             context.events.now.key_escape == Some(true) {
             ViewAction::Quit
         }
         else if context.events.now.key_space == Some(true) {
-            ViewAction::ChangeView(next_view)
+            ViewAction::ChangeView(Box::new(ViewB))
         }
         else {
-            context.renderer.set_draw_color(color);
+            context.renderer.set_draw_color(Color::RGB(255, 0, 0));
             context.renderer.clear();
             ViewAction::None
         }
-}
-
-impl View for ViewA {
-    fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction {
-        render_template(context, elapsed, Color::RGB(255, 0, 0), Box::new(ViewB))
     }
 }
 
 impl View for ViewB {
     fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction {
-        render_template(context, elapsed, Color::RGB(0, 0, 255), Box::new(ViewA))
+        if context.events.now.quit ||
+            context.events.now.key_escape == Some(true) {
+            ViewAction::Quit
+        }
+        else if context.events.now.key_space == Some(true) {
+            ViewAction::ChangeView(Box::new(ViewA))
+        }
+        else {
+            context.renderer.set_draw_color(Color::RGB(0, 0, 255));
+            context.renderer.clear();
+            ViewAction::None
+        }
     }
 }
